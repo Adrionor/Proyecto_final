257d9d00e13c3f52cb6831009c355f9b517db2bd
Adrionor <jorgeadrian.dp@gmail.com>
2023-08-12 16:13:19 -0700

    commit 16:13

--- predictor/predict.py
+++ predictor/predict.py
@@ -1,52 +1,33 @@
 import argparse
 import joblib
 import pandas as pd
+from imblearn.pipeline import make_pipeline as imbalanced_make_pipeline
+from sklearn.pipeline import make_pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import StratifiedKFold
+from sklearn.preprocessing import StandardScaler
 
 class ModelPredictor:
-    """
-    A class to load a trained machine learning model and make predictions on new data.
-
-    Parameters:
-        model_path (str): Path to the trained model file (joblib format).
-
-    Methods:
-        predict(new_data):
-            Makes predictions on the provided new_data using the loaded model.
-
-    Usage:
-        $ python model_predictor.py trained_models/logistic_regression_output.pkl path_to_new_data
-    """
-
+    
     def __init__(self, model_path):
-        """
-        Initializes the ModelPredictor instance.
-
-        Parameters:
-            model_path (str): Path to the trained model file (joblib format).
-        """
         self.model = joblib.load(model_path)
 
     def predict(self, new_data):
-        """
-        Makes predictions on the provided new_data using the loaded model.
-
-        Parameters:
-            new_data: The data on which to make predictions.
-
-        Returns:
-            Predicted outputs from the model.
-        """
+        # Applying StandardScaler
+        SC = StandardScaler()
+        new_data = pd.DataFrame(SC.fit_transform(new_data.values), 
+                                index=new_data.index, columns=new_data.columns)
         return self.model.predict(new_data)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Model Predictor')
     parser.add_argument('model_path', type=str, help='Path to the trained model file')
     parser.add_argument('new_data', type=str, help='Path to the file containing new data for prediction')
+    
     args = parser.parse_args()
 
     predictor = ModelPredictor(args.model_path)
 
-    # Load the new data from the specified CSV file
     new_data = pd.read_csv(args.new_data)
 
     predictions = predictor.predict(new_data)
