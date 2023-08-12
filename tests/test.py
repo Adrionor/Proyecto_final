43af722e303b5faee1ef6bd6beaf7781cdb0ec9e
Adrionor <jorgeadrian.dp@gmail.com>
2023-08-12 16:41:44 -0700

    commit 16:41

--- tests/test.py
+++ tests/test.py
@@ -1,41 +1,43 @@
-
-#Testing
-
+# test.py
 import unittest
-import os
+import pandas as pd
+from load_data import DataLoader
+from preprocess import DataPreprocessor
+from predictor import ModelPredictor
 
-class TestMyProject(unittest.TestCase):
-    def test_training_data_exists(self):
-        # Verify that the training data exists
-        self.assertTrue(os.path.exists('data.csv'))
+class ModelTester(unittest.TestCase):
+    """
+    A class used to test the model.
 
-    # You'll need to provide more information about your custom transformers and their expected behavior
-    # in order to write tests for them.
+    ...
 
-    # def test_custom_transformer_1(self):
-    #     # Validate that MyCustomTransformer1 performs the expected transformations
-    #     transformer = MyCustomTransformer1()
-    #     input_data = ...
-    #     expected_output = ...
-    #     output = transformer.transform(input_data)
-    #     self.assertEqual(output, expected_output)
+    Methods
+    -------
+    test_predictions(self)
+        Tests the predictions made by the model.
+    """
 
-    # def test_custom_transformer_2(self):
-    #     # Validate that MyCustomTransformer2 performs the expected transformations
-    #     transformer = MyCustomTransformer2()
-    #     input_data = ...
-    #     expected_output = ...
-    #     output = transformer.transform(input_data)
-    #     self.assertEqual(output, expected_output)
+    def test_predictions(self):
+        """
+        Tests the predictions made by the model.
+        """
+        # Load test data
+        data_loader = DataLoader('test_data.csv')
+        test_data = data_loader.load_data()
 
-    def test_model_exists(self):
-        # You'll need to provide more information about how your model is trained and saved
-        # in order to write a test for it.
+        # Preprocess test data
+        preprocessor = DataPreprocessor()
+        preprocessed_data = preprocessor.preprocess(test_data)
 
-        # Train the model and save the result
-        # train_model()
-        # Verify that the trained model exists
-        self.assertTrue(os.path.exists('My_model\My_model_regression'))
+        # Load model
+        predictor = ModelPredictor('model.pkl')
+
+        # Make predictions
+        predictions = predictor.predict(preprocessed_data)
+
+        # Perform assertions
+        self.assertEqual(len(predictions), len(test_data))
+        # Add more assertions as needed
 
 if __name__ == '__main__':
     unittest.main()
