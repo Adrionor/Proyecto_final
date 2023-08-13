c28dfba3601cc17fea99ea43cd28f57fdd539f21
Adrionor <jorgeadrian.dp@gmail.com>
2023-08-13 13:18:37 -0700

    commit 13:20

--- my_folder_/Load/load_data.py
+++ my_folder_/Load/load_data.py
@@ -1,41 +1,12 @@
-# load.py
 import pandas as pd
 
 class DataLoader:
-    """
-    A class used to load data from a CSV file.
-
-    ...
-
-    Attributes
-    ----------
-    file_path : str
-        Path to the CSV file.
-
-    Methods
-    -------
-    load_data() -> pd.DataFrame
-        Loads data from the CSV file.
-    """
-
     def __init__(self, file_path: str):
-        """
-        Constructs all the necessary attributes for the DataLoader object.
-
-        Parameters
-        ----------
-            file_path : str
-                Path to the CSV file.
-        """
         self.file_path = file_path
 
     def load_data(self) -> pd.DataFrame:
-        """
-        Loads data from the CSV file.
-
-        Returns
-        -------
-            pd.DataFrame
-                Data loaded from the CSV file.
-        """
         return pd.read_csv(self.file_path)
+
+# Load bankruptcy data
+loader = DataLoader("my_folder_/Data/data.csv")
+data = loader.load_data()
