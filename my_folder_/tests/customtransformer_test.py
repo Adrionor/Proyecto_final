1e2fc380b5f70a095f799a0cf7f83abfd83d4e49
Adrionor <jorgeadrian.dp@gmail.com>
2023-08-13 15:35:40 -0700

    commit 15:35

--- my_folder_/tests/customtransformer_test.py
+++ my_folder_/tests/customtransformer_test.py
@@ -12,8 +12,8 @@ from outliers_removal_transformer import OutliersRemovalTransformer
 data = pd.read_csv('my_folder_\Data\data.csv')
 
 # Split the dataset into features and target
-X = data.drop('target', axis=1)
-y = data['target']
+X = data.drop('Bankrupt?', axis=1)
+y = data['Bankrupt?']
 
 # Split the data into train and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -23,13 +23,3 @@ pipeline = Pipeline([
     ('outliers_removal', OutliersRemovalTransformer()),
     ('classifier', LogisticRegression())
 ])
-
-# Fit the pipeline on the training data
-pipeline.fit(X_train, y_train)
-
-# Make predictions on the test data
-y_pred = pipeline.predict(X_test)
-
-# Calculate the accuracy score
-accuracy = accuracy_score(y_test, y_pred)
-print('Accuracy:', accuracy)
--- my_folder_/train/train.py
+++ my_folder_/train/train.py
@@ -1,10 +1,11 @@
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+from preprocess import DataPreprocessor
 
 class DataLoader:
     def __init__(self, file_path: str):
-        self.file_path = r"my_folder_\Data\data.csv"
+        self.file_path = file_path
 
     def load_data(self) -> pd.DataFrame:
         return pd.read_csv(self.file_path)
@@ -13,6 +14,9 @@ def train_model(data: pd.DataFrame):
     # Split features and labels
     X = data.drop("Bankrupt?", axis=1)
     y = data["Bankrupt?"]
+
+    # Convert continuous target values to discrete classes
+    y = (y > 0.5).astype(int)
 
     # Split data into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -27,7 +31,8 @@ def train_model(data: pd.DataFrame):
 
 def main():
     # Load bankruptcy data
-    loader = DataLoader("bankruptcy_data.csv")
+    file_path = r"my_folder_\Data\data.csv"
+    loader = DataLoader(file_path)
     data = loader.load_data()
 
     # Train the model
