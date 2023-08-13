e84fa62bcbdb8d10556b28071f094c5658a0174c
Adrionor <jorgeadrian.dp@gmail.com>
2023-08-12 19:37:09 -0700

    commit local 19:36

--- main.py
+++ main.py
@@ -1,18 +1,48 @@
 # main.py
-import argparse
+import unittest
 import pandas as pd
-from predict import ModelPredictor
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+import numpy as np
+
+class TestDataExistence(unittest.TestCase):
+    def test_data_existence(self):
+        # Load training data
+        df_train = pd.read_csv(r'Data\data.csv')
+
+        # Verify training data existance
+        self.assertFalse(df_train.empty, "No training data")
+
+class InputData(BaseModel):
+    feature1: float
+    feature2: float
+    # Add more features as needed
+
+class OutputData(BaseModel):
+    prediction: int
+
+model = joblib.load("My_model\My_model_regression")
+
+app = FastAPI()
+
+@app.post("/train")
+def train_model():
+    # Implement the logic to train a new model
+    # Return a response indicating the success or failure of the training process
+    pass
+
+@app.post("/predict", response_model=OutputData)
+def predict(input_data: InputData):
+    # Convert the input data to a numpy array
+    input_array = np.array([[input_data.feature1, input_data.feature2]])
+    
+    # Make predictions using the loaded model
+    prediction = model.predict(input_array)
+    
+    # Create an instance of the output data model and return it
+    output_data = OutputData(prediction=int(prediction[0]))
+    return output_data
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Model Predictor')
-    parser.add_argument('model_path', type=str, help='Path to the trained model file')
-    parser.add_argument('new_data', type=str, help='Path to the file containing new data for prediction')
-    
-    args = parser.parse_args()
-
-    predictor = ModelPredictor(args.model_path)
-
-    new_data = pd.read_csv(args.new_data)
-
-    predictions = predictor.predict(new_data)
-    print(predictions)
+    unittest.main()
