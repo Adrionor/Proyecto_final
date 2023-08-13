1959a2b702a0eee79a1c47f79dbdf3b62bfb6617
Adrionor <jorgeadrian.dp@gmail.com>
2023-08-12 20:17:59 -0700

    commit 20:17

---
+++ tests/customtransformer_test.py
@@ -0,0 +1,34 @@
+#customtransformer test
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from outliers_removal_transformer import OutliersRemovalTransformer
+
+# Load the dataset
+data = pd.read_csv('Data\data.csv')
+
+# Split the dataset into features and target
+X = data.drop('target', axis=1)
+y = data['target']
+
+# Split the data into train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create the pipeline with the custom transformer
+pipeline = Pipeline([
+    ('outliers_removal', OutliersRemovalTransformer()),
+    ('classifier', LogisticRegression())
+])
+
+# Fit the pipeline on the training data
+pipeline.fit(X_train, y_train)
+
+# Make predictions on the test data
+y_pred = pipeline.predict(X_test)
+
+# Calculate the accuracy score
+accuracy = accuracy_score(y_test, y_pred)
+print('Accuracy:', accuracy)
